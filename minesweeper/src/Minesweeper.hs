2bda863225879b5620f71374109b8679ae1f4335
Myoungjin JEON <jeongoon@gmail.com>
2022-06-16 22:40:48 +1000

    add imcomplete "minesweeper"

--- minesweeper/src/Minesweeper.hs
+++ minesweeper/src/Minesweeper.hs
@@ -1,15 +1,21 @@
 module Minesweeper (minesweeperRange, annotate) where
 
 minesweeperRange :: [a] -> [[a]]
-minesweeperRange ms =
-  let
-    minesweeperRange' acc ms =
-      let
-        ks = take 3 ms
-        rest = drop (length ks) ms
-      in
-        if 
-        
+minesweeperRange =
+  let (acc, lastRange) = foldl
+      (\(acc, prev) m ->
+         let
+           new =
+             if length prev < 3 then
+               prev ++ [m]
+             else
+               (tail prev) ++ [m]
+         in
+           (new:acc, new)
+        ) ([take 2 ms], (drop 2 ms))
+  in
+    (reverse acc) ++ [
+
 
 --annotate :: [String] -> [String]
 annotate board =
