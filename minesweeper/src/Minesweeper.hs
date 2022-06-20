863daf9cbcd86304852173479ecff72c9b13b777
Myoungjin JEON <jeongoon@gmail.com>
2022-06-20 22:14:15 +1000

    first iteration for "minesweeper"

--- minesweeper/src/Minesweeper.hs
+++ minesweeper/src/Minesweeper.hs
@@ -1,25 +1,54 @@
-module Minesweeper (minesweeperRange, annotate) where
+module Minesweeper (minesweeperRange, countMines, annotate) where
+
+import Data.List (transpose)
+import Data.Char (intToDigit)
 
 minesweeperRange :: [a] -> [[a]]
-minesweeperRange =
-  let (acc, lastRange) = foldl
-      (\(acc, prev) m ->
-         let
-           new =
-             if length prev < 3 then
-               prev ++ [m]
-             else
-               (tail prev) ++ [m]
-         in
-           (new:acc, new)
-        ) ([take 2 ms], (drop 2 ms))
-  in
-    (reverse acc) ++ [
+minesweeperRange ms
+  | null ms = [[]]
+  | otherwise =
+      let
+        first':rest' = ms
+        first:rest = minesweeperRange' $ first':first':rest'
+                                       -- ^^^^ dummy: will be removed in the end
+      in
+        (drop 1 first) : rest
+  where
+    minesweeperRange' begs -- begs: the group of beginning members
+      | null mids = []
+      | otherwise =
+          concat (map (take 1) [begs, mids, ends])
+          : minesweeperRange' mids
+      where
+        mids = drop 1 begs
+        ends = drop 1 mids
 
+countMines :: String -> Int
+countMines =
+  foldr (\x acc -> if '*' == x then
+                     acc+1
+                   else
+                     acc) 0
 
---annotate :: [String] -> [String]
+annotate :: [String] -> [String]
 annotate board =
   let
-    rowGroups = minesweeperRange board
+    makeGroups = transpose . map minesweeperRange
+    eachAreaMines :: [[Int]]
+    eachAreaMines =
+      map (map (countMines . concat))
+      . makeGroups
+      . makeGroups
+      $ board
   in
-    rowGroups
+    zipWith
+    (\as bs ->
+       zipWith
+      (\a b ->
+         case (a, b) of
+           (_, '*') -> '*'
+           (0, _) -> ' '
+           (_, _) -> intToDigit a
+      )
+      as bs)
+    eachAreaMines board
