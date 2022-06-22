edf8624a7010344f4d9c45bff5f726dabeb5bad8
Myoungjin JEON <jeongoon@gmail.com>
2022-06-22 22:22:06 +1000

    change the order of grouping to use less `transpose` more `map`

--- minesweeper/src/Minesweeper.hs
+++ minesweeper/src/Minesweeper.hs
@@ -1,54 +1,43 @@
-module Minesweeper (minesweeperRange, countMines, annotate) where
+module Minesweeper where
 
 import Data.List (transpose)
 import Data.Char (intToDigit)
 
-minesweeperRange :: [a] -> [[a]]
-minesweeperRange ms
-  | null ms = [[]]
-  | otherwise =
-      let
-        first':rest' = ms
-        first:rest = minesweeperRange' $ first':first':rest'
-                                       -- ^^^^ dummy: will be removed in the end
-      in
-        (drop 1 first) : rest
+mineWindow :: [a] -> [[a]]
+mineWindow [] = [[]]
+mineWindow (first':rest') =
+  let
+    first:rest = mineWindow' $ first':first':rest'
+                                     -- ^^^^ dummy: will be removed in the end
+  in
+    (drop 1 first) : rest
   where
-    minesweeperRange' begs -- begs: the group of beginning members
-      | null mids = []
-      | otherwise =
-          concat (map (take 1) [begs, mids, ends])
-          : minesweeperRange' mids
-      where
-        mids = drop 1 begs
-        ends = drop 1 mids
+    mineWindow' (beg : mids@(mid : ends)) =
+      (if null ends then  [ beg, mid ]
+       else               [ beg, mid, head ends ]) : mineWindow' mids
+    mineWindow' _ = []
 
 countMines :: String -> Int
-countMines =
-  foldr (\x acc -> if '*' == x then
-                     acc+1
-                   else
-                     acc) 0
+countMines = foldr (\x acc -> if '*' == x then acc + 1 else acc) 0
 
 annotate :: [String] -> [String]
 annotate board =
   let
-    makeGroups = transpose . map minesweeperRange
-    eachAreaMines :: [[Int]]
-    eachAreaMines =
-      map (map (countMines . concat))
-      . makeGroups
-      . makeGroups
-      $ board
+    eachAreaMines = ( map $
+                      map (countMines . concat)
+                      . transpose
+                      . map mineWindow
+                        -- ^ creating column groups by each row and count
+                    )
+                    . mineWindow $ -- creating row groups by row
+
+                    board
   in
     zipWith
-    (\as bs ->
-       zipWith
-      (\a b ->
-         case (a, b) of
-           (_, '*') -> '*'
-           (0, _) -> ' '
-           (_, _) -> intToDigit a
-      )
-      as bs)
-    eachAreaMines board
+    ( zipWith (\count cell ->
+                 case (count, cell) of
+                   (_, '*') -> '*'
+                   (0, _) -> ' '
+                   (_, _) -> intToDigit count
+              )
+    ) eachAreaMines board
