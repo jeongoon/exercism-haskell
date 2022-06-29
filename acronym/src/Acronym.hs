d2bd3bb5e98934c3fe1e71a857869c97937ffec4
Myoungjin JEON <jeongoon@gmail.com>
2022-06-30 09:32:18 +1000

    update "acronym"; change condition; remove uncurry

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -3,23 +3,24 @@ module Acronym (abbreviate) where
 import           Data.Char as C
 
 abbreviate :: String -> String
-abbreviate = (uncurry consHelper)
-             . foldr (\c (mbCandi, abbr) ->
+abbreviate = consHelper
+             . foldr (\c origAcc@(mbCandi, abbr) ->
                           if C.isAlpha c then
-                            if (not . C.isUpper $ c)
-                               || (C.isUpper <$> mbCandi) /= (Just False) then
-                                ( Just c, abbr )
-                            else
+                            if C.isUpper c
+                               && (C.isUpper <$> mbCandi) == (Just False) then
                                 ( Nothing, c : abbr )
+                            else
+                                ( Just c, abbr )
 
                           else if c == '\'' then
                                  ( mbCandi, abbr )
                                else
                                  ( Nothing
-                                 , consHelper mbCandi abbr )
-                       )
+                                 , consHelper origAcc
+                                 )
+                     )
              (Nothing, "")
   where
-    consHelper mbChar acc = case mbChar of
-                              Just ch -> (C.toUpper ch) : acc
-                              Nothing -> acc
+    consHelper (mbChar, acc) = case mbChar of
+                                 Just ch -> (C.toUpper ch) : acc
+                                 Nothing -> acc
