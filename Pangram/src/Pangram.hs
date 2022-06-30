e1c189cf3ea3a5da5c162acde7e1676746174e83
Myoungjin JEON <jeongoon@gmail.com>
2022-06-30 19:19:35 +1000

    make more versions of "Pangram"

--- Pangram/src/Pangram.hs
+++ Pangram/src/Pangram.hs
@@ -1,28 +1,46 @@
 module Pangram (isPangram) where
 
 import qualified Data.Char as Char
---import qualified Data.Set as Set
+import qualified Data.Set as Set
+import Data.List (nub)
 
-isPangram :: String -> Bool
-isPangram =
-  (26 ==)
-  . length
-  . (foldr sieve "")
+isPangram = isPangramCustom
+
+isPangramSet :: String -> Bool
+isPangramSet =
+  (26==)
+  . Set.size
+  . foldr sieve Set.empty
 
   where
-    sieve ch acc =
-      let lch = Char.toLower ch
+    isLowerAlhpa26 lch = -- lch : lower character
+        'a' <= lch && lch <= 'z'
+
+    sieve c acc =
+      let lc = Char.toLower c
       in
-        if isLowerAlhpa26 lch &&
-           (not . (ch `elem`) $ acc) then
-          lch : acc
+        if isLowerAlhpa26 lc then
+          (Set.insert lc acc)
         else
           acc
 
-    ord_a = Char.ord 'a'
-    ord_z = Char.ord 'z'
-    isLowerAlhpa26 ch =
+isPangramCustom :: String -> Bool
+isPangramCustom =
+  (26 ==)
+  . length
+  . take 26
+  . accPangram' ""
 
-      let och = Char.ord ch
+  where
+    accPangram' acc [] = []
+    accPangram' acc (c:cs) =
+      let lc = Char.toLower c
       in
-        ord_a <= och && och <= ord_z
+        if isLowerAlhpa26 lc
+           && (not . elem lc $ acc) then
+          True : accPangram' (lc:acc) cs
+        else
+          accPangram' acc cs
+
+    isLowerAlhpa26 lch =
+        'a' <= lch && lch <= 'z'
